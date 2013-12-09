9f6162a9e88f825525eb689810558bd587ed8bc7
Stefan Partheymüller <stefan-partheymueller@gmx.de>
2013-12-09 21:24:11 +0100

    updated unit-test script

--- test.sh
+++ test.sh
@@ -13,21 +13,48 @@ function check_equals()
 	
 	# perform check
 	if [ $1 -eq $2 ]; then
-		echo "-> CHECK: OK"
+		echo "-> CHECK: OK      ($checks)"
 		(( successes += 1 ))
-		return 0;
+		return 0
 	else
-		echo "-> CHECK: FAILED"
+		echo "-> CHECK: FAILED  ($checks)"
 		(( failures += 1 ))
 		echo "   exp: $1, act: $2"
+		echo "   expression: \`$3'"
+		echo ""
+		return 1
+	fi
+}
+function check_equals_text()
+{
+	# increase check-count
+	(( checks += 1 ))
+	
+	# perform check
+	if [ "$1" == "$2" ]; then
+		echo "-> CHECK: OK      ($checks)"
+		(( successes += 1 ))
+		return 0
+	else
+		echo "-> CHECK: FAILED  ($checks)"
+		(( failures += 1 ))
+		echo "   exp: '$1', act: '$2'"
+		echo ""
 		return 1
 	fi
 }
 
 function check_cbc()
 {
-	cbc_result=`echo "$2," | ./cbc`
-	check_equals $1 $cbc_result
+	expression="$2,"
+	cbc_result=`echo "$expression" | ./cbc`
+	check_equals $1 "$cbc_result" "$expression"
+}
+
+function check_cbc_text()
+{
+	cbc_result=`echo "$2" | ./cbc 2>&1`
+	check_equals_text "$1" "$cbc_result"
 }
 
 ################################################################################
@@ -36,16 +63,21 @@ function check_cbc()
 #print header
 echo "running cbc unit-test ..."
 echo "-------------------------"
+
 # checks
-check_cbc 5		"2 + 3"
-check_cbc 17	"50 - 33"
-check_cbc -1	"2 - 3"
-check_cbc 6		"2 * 3"
-check_cbc 259	"777 / 3"
-check_cbc 0		""
-check_cbc 13	"3 + 4 * 3 - 8 / 4"
-check_cbc -7	"(3 + 4) * (3 - 8) / 5"
-check_cbc 15	"-3 * -5"
+check_cbc	5		"2 + 3"
+check_cbc	17		"50 - 33"
+check_cbc	-1		"2 - 3"
+check_cbc	6		"2 * 3"
+check_cbc	259		"777 / 3"
+check_cbc	13		"3 + 4 * 3 - 8 / 4"
+check_cbc	-7		"(3 + 4) * (3 - 8) / 5"
+check_cbc	15		"-3 * -5"
+check_cbc	-100	"-100"
+check_cbc	130		"3 + 5 * 11 - (80 - 14) / 33 * 2 - 50 + 3 * 7 * (66 / 11)"
+# check syntax errors
+check_cbc_text	"syntax error"	","
+
 echo "-------------------------"
 echo "EXECUTED CHECKS: $checks"
 echo "SUCCESSFUL:      $successes"
