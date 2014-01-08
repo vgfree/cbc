c7d8139b79cacc6a73ca23db2532ea24f8fd6535
Stefan Partheymüller <stefan-partheymueller@gmx.de>
2014-01-09 00:41:16 +0100

    removed unneeded includes

--- syntree.c
+++ syntree.c
@@ -3,7 +3,6 @@
  ******************************************************************************/
 
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 #include "syntree.h"
 #include "symref.h"
