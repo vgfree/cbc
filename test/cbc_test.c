12ad38ab00c4d6349b5ad24f01c9997b4e455ecb
Stefan Partheymüller <stefan-partheymueller@gmx.de>
2014-01-23 00:47:51 +0100

    reimplemented unit-test to use the sput-framework

--- test/cbc_test.c
+++ test/cbc_test.c
@@ -1,73 +1,57 @@
 /*******************************************************************************
- * cbc_test -- Unit-Test for cbc using the 'Check'-framework.
+ * cbc_test -- Codeblock Unit-Test using the 'sput' Unit-Test framework
  ******************************************************************************/
 
+#include <string.h>
+
 #include "cbc_test.h"
+
 #include "../codeblock.h"
 #include "../syntree.h"
 #include "../symtab.h"
 
 
 // #############################################################################
-// test-functions
+// test procedures
 // #############################################################################
 
-
 // -----------------------------------------------------------------------------
-// codeblock test-function
+// Test: codeblock_execute()
 // -----------------------------------------------------------------------------
-START_TEST (codeblock_test)
+static void test_codeblock_execute()
 {
-	// set up
 	codeblock_t* cb		= codeblock_create();
+	cb->ast				= conststr_create("test");
 	cb->global_symtab	= symtab_create();
 	
-	cb->ast = conststr_create("test");	// set codeblock-code
-	codeblock_execute(cb);				// execute ...
+	codeblock_execute(cb);	// execute codeblock once
 	
+	sput_assert(cb->result->type					== VT_STRING);
+	sput_assert(strcmp(cb->result->string, "test")	== 0);
 	
-	// checks
-	ck_assert(cb->result->type					== VT_STRING);
-	ck_assert(strcmp(cb->result->string, "test")== 0);
+	codeblock_execute(cb);	// execute a second time 
 	
+	sput_assert(cb->result->type					== VT_STRING);
+	sput_assert(strcmp(cb->result->string, "test")	== 0);
 	
-	// tear down
 	syntree_free(cb->ast);
 	symtab_free(cb->global_symtab);
 	codeblock_free(cb);
 }
-END_TEST
-
-// -----------------------------------------------------------------------------
-// codeblock test-suite
-// -----------------------------------------------------------------------------
-Suite* codeblock_suite()
-{
-	Suite* suite = suite_create("codeblock");
-	
-	TCase* tc_codeblock = tcase_create("codeblock");
-	tcase_add_test(tc_codeblock, codeblock_test);
-	suite_add_tcase(suite, tc_codeblock);
-	
-	return suite;
-}
 
 
 // #############################################################################
-// main
+// Main
 // #############################################################################
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	int failed;
+	sput_start_testing();	// *************************************************
 	
-	Suite* cb_suite		= codeblock_suite();
-	SRunner* cb_runner	= srunner_create(cb_suite);
+	sput_enter_suite("Execute simple codeblock");
+	sput_run_test(test_codeblock_execute);
 	
-	srunner_run_all(cb_runner, CK_NORMAL);
+	sput_finish_testing();	// *************************************************
 	
-	failed = srunner_ntests_failed(cb_runner);
-	srunner_free(cb_runner);
-	
-	return failed;
+	return sput_get_return_value();
 }
--- test/cbc_test.h
+++ test/cbc_test.h
@@ -1,12 +1,20 @@
 /*******************************************************************************
- * cbc_test -- Unit-Test for cbc using the 'Check'-framework.
+ * cbc_test -- Codeblock Unit-Test using the 'sput' Unit-Test framework
  ******************************************************************************/
 
 #ifndef CBC_TEST_H
 #define CBC_TEST_H
 
 
-#include <check.h>
+#include <sput.h>
+
+// helper-macros
+#define S(x) #x
+#define S_(x) S(x)
+
+// custom sput-framework additions
+#define sput_assert(expr)			sput_fail_if(!expr, __FILE__ ":" S_(__LINE__))
+#define sput_assert_msg(expr, msg)	sput_fail_if(!expr, msg)
 
 
 #endif // CBC_TEST_H
