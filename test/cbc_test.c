0befa7023e17bce3860e54fc1fa6f5d3c1c407b7
Stefan Partheymüller <stefan-partheymueller@gmx.de>
2014-01-28 11:08:16 +0100

    fixed memory-leaks in unit-test

--- test/cbc_test.c
+++ test/cbc_test.c
@@ -7,23 +7,44 @@
 #include "codeblock_test.h"
 #include "scope_test.h"
 
+
+// #############################################################################
+// helper functions
+// #############################################################################
+
+// -----------------------------------------------------------------------------
+// wrapper for CuSuiteAddSuite()
+// -----------------------------------------------------------------------------
+void CuSuiteAddSuite_Custom(CuSuite* testSuite, CuSuite* testSuite2)
+{
+	CuSuiteAddSuite(testSuite, testSuite2);
+	free(testSuite2);	// free the dummy test-suite
+}
+
+
 // #############################################################################
 // Main
 // #############################################################################
 
 int main(int argc, char *argv[])
 {
-	CuString *output= CuStringNew();
+	CuString* output= CuStringNew();
 	CuSuite* suite	= CuSuiteNew();
 	
-	CuSuiteAddSuite(suite, make_suite_codeblock());
-	CuSuiteAddSuite(suite, make_suite_scope());
+	CuSuiteAddSuite_Custom(suite, make_suite_codeblock());
+	CuSuiteAddSuite_Custom(suite, make_suite_scope());
 	
+	// run tests
 	CuSuiteRun(suite);
 	CuSuiteSummary(suite, output);
 	CuSuiteDetails(suite, output);
 	
-	printf("%s\n", output->buffer);
+	printf("%s\n", output->buffer);	// print test-result
+	int result = suite->failCount;
 	
-	return suite->failCount;
+	// cleanup test-environment
+	CuStringDelete(output);
+	CuSuiteDelete(suite);
+	
+	return result;
 }
