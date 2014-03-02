6460b75d84ff0e516ae55d07f524d5e3946d46e3
Stefan Partheymüller <stefan-partheymueller@gmx.de>
2014-03-03 00:37:05 +0100

    implemented unit-test for syntax errors

--- test/error_handling_test.c
+++ test/error_handling_test.c
@@ -14,9 +14,11 @@
 
 static const char codeblock_string_undeffunc[] = "UndefinedFunction(),";
 static const char codeblock_string_undefvar[] = "cUndefinedVar := 'foobar',";
+static const char codeblock_string_unexptokennum[] = "WriteLn('foo' 123, True),";
+static const char codeblock_string_missingcomma[] = "WriteLn('foo')";
 
-static void test_runtime_error(CuTest* tc, const char* codeblock_string,
-							   const char* expected_error_message);
+static void test_error(CuTest* tc, const char* codeblock_string,
+					   const char* expected_error_message, cb_error_type type);
 static void stream_to_string_helper(FILE* stream, char* string);
 
 
@@ -25,20 +27,25 @@ static void stream_to_string_helper(FILE* stream, char* string);
 // #############################################################################
 
 // -----------------------------------------------------------------------------
-// Generic test procedure to test runtime errors
+// Generic test procedure to test errors
 // -----------------------------------------------------------------------------
-static void test_runtime_error(CuTest* tc, const char* codeblock_string,
-							   const char* expected_error_message)
+static void test_error(CuTest* tc, const char* codeblock_string,
+					   const char* expected_error_message, cb_error_type type)
 {
-	Codeblock* cb = codeblock_create();
-	
-	CuAssertIntEquals(tc, EXIT_SUCCESS,
-					  codeblock_parse_string(cb, codeblock_string));
-	
 	FILE* err_out = tmpfile();
 	cb_set_error_output(err_out);
 	
-	CuAssertIntEquals(tc, EXIT_FAILURE, codeblock_execute(cb));
+	Codeblock* cb = codeblock_create();
+	
+	int expected_parsing_result = EXIT_SUCCESS;
+	if (type == CB_ERR_SYNTAX)
+		expected_parsing_result = EXIT_FAILURE;
+	
+	CuAssertIntEquals(tc, expected_parsing_result,
+					  codeblock_parse_string(cb, codeblock_string));
+	
+	if (type != CB_ERR_SYNTAX)
+		CuAssertIntEquals(tc, EXIT_FAILURE, codeblock_execute(cb));
 	
 	char stream_content[512];	// allocating 512 bytes should be enough for an
 								// error message
@@ -46,9 +53,21 @@ static void test_runtime_error(CuTest* tc, const char* codeblock_string,
 	
 	CuAssertStrEquals(tc, expected_error_message, stream_content);
 	
-	fclose(err_out);
-	
+	fclose(err_out),
 	codeblock_free(cb);
+}
+
+// -----------------------------------------------------------------------------
+// Test syntax errors
+// -----------------------------------------------------------------------------
+void test_error_handling_syntax_errors(CuTest *tc)
+{
+	test_error(tc, codeblock_string_unexptokennum,
+			   "Syntax error: Line 1: Unexpected token NUMBER\n",
+			   CB_ERR_SYNTAX);
+	test_error(tc, codeblock_string_missingcomma,
+			   "Syntax error: Line 1: Unexpected token ENDOFFILE\n",
+			   CB_ERR_SYNTAX);
 }
 
 // -----------------------------------------------------------------------------
@@ -56,10 +75,12 @@ static void test_runtime_error(CuTest* tc, const char* codeblock_string,
 // -----------------------------------------------------------------------------
 void test_error_handling_undefinedsymbol(CuTest *tc)
 {
-	test_runtime_error(tc, codeblock_string_undeffunc,
-					   "Runtime error: Line 1: Undefined symbol: UndefinedFunction\n");
-	test_runtime_error(tc, codeblock_string_undefvar,
-					   "Runtime error: Line 1: Undefined symbol: cUndefinedVar\n");
+	test_error(tc, codeblock_string_undeffunc,
+			   "Runtime error: Line 1: Undefined symbol: UndefinedFunction\n",
+			   CB_ERR_RUNTIME);
+	test_error(tc, codeblock_string_undefvar,
+			   "Runtime error: Line 1: Undefined symbol: cUndefinedVar\n",
+			   CB_ERR_RUNTIME);
 }
 
 
@@ -70,6 +91,7 @@ void test_error_handling_undefinedsymbol(CuTest *tc)
 CuSuite* make_suite_error_handling()
 {
 	CuSuite* suite = CuSuiteNew();
+	SUITE_ADD_TEST(suite, test_error_handling_syntax_errors);
 	SUITE_ADD_TEST(suite, test_error_handling_undefinedsymbol);
 	return suite;
 }
