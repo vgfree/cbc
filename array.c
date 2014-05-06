b16e1837c4242fc0ec9d59ba583960a2a2c52e95
Stefan Partheymüller <stefan-partheymueller@gmx.de>
2014-05-06 17:14:27 +0200

    first implementation of the dynamic array

---
+++ array.c
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ * CbArray -- Generic implementation of a dynamic array structure
+ ******************************************************************************/
+
+#include <stdlib.h>
+#include "array.h"
+
+
+// #############################################################################
+// declarations
+// #############################################################################
+
+struct CbArray
+{
+	size_t count;
+	CbArrayItem* elements;
+	int block_size;
+	int alloc_size;
+};
+
+static bool cb_array_is_full(CbArray* array);
+
+
+// #############################################################################
+// interface-functions
+// #############################################################################
+
+// -----------------------------------------------------------------------------
+// Constructor
+// -----------------------------------------------------------------------------
+CbArray* cb_array_create()
+{
+	CbArray* array    = (CbArray*) malloc(sizeof(CbArray));
+	array->count      = 0;
+	// default block size is the size of 16 elements
+	array->block_size = sizeof(CbArrayItem) * 16;
+	array->elements   = (CbArrayItem*) malloc(array->block_size);
+	array->alloc_size = array->block_size;
+	
+	return array;
+}
+
+// -----------------------------------------------------------------------------
+// Destructur
+// -----------------------------------------------------------------------------
+void cb_array_free(CbArray* array)
+{
+	free(array->elements);
+	free(array);
+}
+
+// -----------------------------------------------------------------------------
+// Get count of elements in array
+// -----------------------------------------------------------------------------
+size_t cb_array_get_count(CbArray* array)
+{
+	return array->count;
+}
+
+// -----------------------------------------------------------------------------
+// Append element to array
+// -----------------------------------------------------------------------------
+bool cb_array_append(CbArray* array, const CbArrayItem item)
+{
+	if (cb_array_is_full(array))
+	{
+		// resize
+		CbArrayItem* temp = realloc(array->elements,
+		                            array->alloc_size + array->block_size);
+		if (temp == NULL)
+			return false;
+		
+		array->elements   = temp;
+		array->alloc_size = array->alloc_size + array->block_size;
+	}
+	
+	array->count++;
+	return cb_array_set(array, (array->count - 1), item);
+}
+
+// -----------------------------------------------------------------------------
+// Set element in array
+// -----------------------------------------------------------------------------
+bool cb_array_set(CbArray* array, int index, const CbArrayItem item)
+{
+	if (array->count <= index)
+		return false;
+	
+	array->elements[index] = item;
+	return true;
+}
+
+// -----------------------------------------------------------------------------
+// Set element in array
+// -----------------------------------------------------------------------------
+const CbArrayItem cb_array_get(CbArray* array, int index)
+{
+	if (array->count <= index)
+		return NULL;
+	
+	return array->elements[index];
+}
+
+
+// #############################################################################
+// interface-functions
+// #############################################################################
+
+// -----------------------------------------------------------------------------
+// Check if array is full
+// -----------------------------------------------------------------------------
+static bool cb_array_is_full(CbArray* array)
+{
+	return ((array->count * sizeof(CbArrayItem)) >= array->alloc_size);
+}
---
+++ array.h
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * CbArray -- Generic implementation of a dynamic array structure
+ ******************************************************************************/
+
+#ifndef ARRAY_H
+#define ARRAY_H
+
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include "value.h"
+
+// CbArray struct
+typedef struct CbArray CbArray;
+typedef CbValue* CbArrayItem;
+
+
+// interface functions
+CbArray* cb_array_create();
+void cb_array_free(CbArray* array);
+
+size_t cb_array_get_count(CbArray* array);
+
+bool cb_array_set(CbArray* array, int index, const CbArrayItem item);
+const CbArrayItem cb_array_get(CbArray* array, int index);
+bool cb_array_append(CbArray* array, const CbArrayItem item);
+void cb_array_insert(CbArray* array, int index, const CbArrayItem item);
+void cb_array_remove(CbArray* array, int index);
+
+
+#endif // ARRAY_H
