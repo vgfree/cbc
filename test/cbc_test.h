1b2a06d6f79428e139c5bc535ed76ebecde46857
Stefan Partheymüller <stefan-partheymueller@gmx.de>
2014-05-08 00:31:38 +0200

    added include guards

--- test/cbc_test.h
+++ test/cbc_test.h
@@ -2,6 +2,10 @@
  * cbc_test -- Codeblock Unit-Test using the 'CuTest' Unit-Test framework
  ******************************************************************************/
 
+#ifndef CBC_TEST_H
+#define CBC_TEST_H
+
+
 #include <stdio.h>
 #include <CuTest.h>
 
@@ -19,3 +23,6 @@ extern CuSuite* make_suite_codeblock_generic();
 extern CuSuite* make_suite_syntree();
 extern CuSuite* make_suite_error_handling();
 extern CuSuite* make_suite_array();
+
+
+#endif // CBC_TEST_H
